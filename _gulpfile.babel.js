7e0727efe7017c787108ded50b6f8dbdfdb13148
jbubsk <jbubsk@gmail.com>
2015-11-22 20:42:34 +0300

    Delete _gulpfile.babel.js

--- _gulpfile.babel.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-require('babel-core/register');
-
-var gulp = require('gulp'),
-    $ = require('gulp-load-plugins')();
-
-//require('./gulp/test')(gulp, $);
-require('./gulp/serve')(gulp, $);
